6f4af3c099c32f383079424dc2604c258d19179c
DyutiKole009 <185676938+DyutiKole009@users.noreply.github.com>
2025-12-15 10:03:34 +0530

    Delete frontend/notes-app/src/components/ProtectedRoute.jsx

--- frontend/notes-app/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const ProtectedRoute = ({ isLoggedIn }) => {
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
